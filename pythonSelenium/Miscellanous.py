553d57bad812375ccb7725a2b230f8157d1416c9
Bharti Sagar <bhartisagar93.bs@gmail.com>
2025-02-09 15:32:09 -0500

    Add files via upload

---
+++ pythonSelenium/Miscellanous.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+driver = webdriver.Chrome()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+chrome_options.add_argument("--ignore-certificate-errors")
+
+
+driver.implicitly_wait(2)
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
+driver.get_screenshot_as_file("screen.png")
